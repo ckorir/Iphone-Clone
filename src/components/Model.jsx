553b4967ecc8bbe213954a864517e333f235905c
ckorir <chrisruto.k@gmail.com>
2024-07-29 12:16:25 +0300

    Mapped models and added color container

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -3,6 +3,7 @@ import gsap from 'gsap'
 import React, { useRef, useState } from 'react'
 import ModelView from './ModelView'
 import { yellowImg } from '../utils/Index'
+import { models } from '../constants/Index'
 
 import * as THREE from 'three'
 import { View } from '@react-three/drei'
@@ -74,7 +75,20 @@ const Model = () => {
             >
               <View.Port />
             </canvas>
+          </div>
 
+          <div className='mx-auto w-full'>
+              <p className='text-sm font-light text-center mb-5' >{model.title}</p>
+              <div className='flex-center'>
+                <ul className='color-container'>
+                  {models.map((item, i) => (
+                    <li key={i} className='w-6 h-6 rounded-full mx-2'
+                    style={{backgroundColor: item.color[0]}}
+                    onClick={() => setModel(item)}>
+                    </li>
+                  ))}
+                </ul>
+              </div>
           </div>
         </div>
       </div>
